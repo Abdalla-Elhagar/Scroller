928568e2ae01dadc2add2dc3082fae0207600d40
Nano365 <134546609+Nano365@users.noreply.github.com>
2023-11-03 16:27:51 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let scroller = document.querySelector(".scroller");
+let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll" , ()=>{
+    let scrollTop = document.documentElement.scrollTop;
+    scroller.style.width = `${(scrollTop / height) * 100}%`;
+})
